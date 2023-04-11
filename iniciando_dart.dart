2e47c45483b08f3c000c24c5a4e185bafe2c4209
Rodrigo Gomes <cklrod@hotmail.com>
2023-04-11 17:14:18 -0300

    repetições

--- iniciando_dart.dart
+++ iniciando_dart.dart
@@ -1,25 +1,38 @@
 void main(List<String> args) {
-  double limitCredit = 200;
-  double usedCredit = 70.5;
-  limitCredit -= usedCredit;
-  double newCredit = limitCredit - 70.5;
-  bool balanceCredit = (newCredit > 0);
-  String balanceResult = (balanceCredit ? 'positivo' : 'negativo');
+  // double limitCredit = 200;
+  // double usedCredit = 70.5;
+  // limitCredit -= usedCredit;
+  // double newCredit = limitCredit - 70.5;
+  // bool balanceCredit = (newCredit > 0);
+  // String balanceResult = (balanceCredit ? 'positivo' : 'negativo');
 
   const String name = 'Rodrigo';
   double height = 1.76;
   bool working = true;
   int birth = 1992;
   int age = DateTime.now().year - birth;
+  bool legalAge;
   final surname;
   surname = 'Careca';
+
+  if (age > 18) {
+    legalAge = true;
+  } else {
+    legalAge = false;
+  }
 
   List<dynamic> info = [name, height, working, age, surname];
 
   String frase = 'Eu ne chamo ${info[0]}, ' +
-      'tenho ${info[1]} de altura. ' +
-      'Se trabalho? ${info[2]}. ' +
-      'Tenho ${info[3]} anos, meu apelido é ${info[4]}';
+      'tenho ${info[1]} de altura.\n' +
+      'Se trabalho? ${info[2]}.\n' +
+      'Tenho ${info[3]} anos, meu apelido é ${info[4]}.\n' +
+      'Se sou maior de idade? $legalAge.\n';
 
   print(frase);
+  print('Estou andando');
+
+  for (int i = 1; i <= 5; i++) {
+    print('Passo $i...');
+  }
 }
